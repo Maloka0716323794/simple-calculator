06b7268371cef878399da9a3767bb0a6a7474f64
Gillian <maloka9405@gmail.com>
2021-06-15 19:18:57 +0200

    finished the calculator and added read me file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # simple-calculator
+
+This program is a simple calculator that performs simple arithmetic operations on numbers. 
+This simple calculator can do only addition, subtraction, multiplication, division and remainder.
+
+In the simple calculator program, the add function is used to find the sum of two numbers.
+
+*The function is declared with two parameters x and y.
+*The function is called using its name and passing two arguments 24 and 16 in and the total is of two values is displayed in the console.
--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 function add(x, y) {
     let sum = x + y;
-    console.log(sum);
+    console.log(x+ " + "+y+ " = "+sum);
 }
-
 add(5, 3);
+
+// Substraction
+function subtract(x,y)
+{
+    let difference= x-y;
+    console.log(x+ " - "+y+ " = " +difference);
+}
+subtract(20,7);
+
+
+//Multiplication
+function multiply(x,y)
+{
+    let product= x*y;
+    console.log(x+ " * "+y+ " = "+product);
+}
+multiply(2,4)
+
+ //division
+ function divide(x,y)
+{
+    let quotient= x/y;
+    console.log(x+ " / "+y+ " = "+quotient);
+}
+divide(8,4)
+
+//Remainder
+function modulus(x,y)
+{
+    let remainder= x%y;
+    console.log(x+ " % "+y+ " = "+remainder);
+}
+ modulus(17,4)
